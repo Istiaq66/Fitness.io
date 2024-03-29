522a5bacf0f96f360b231670bfde10fc3a94ebff
istiaq75 <istiaq.6amtech@gmail.com>
2024-03-30 00:14:40 +0600

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_home_app/presentation/auth/providers/auth_provider.dart';
+import 'package:smart_home_app/presentation/consumption/providers/consumption_provider.dart';
+import 'package:smart_home_app/presentation/home/providers/home_provider.dart';
+import 'package:smart_home_app/presentation/profile/providers/profile_provider.dart';
+import 'package:smart_home_app/presentation/settings/providers/settings_provider.dart';
+import 'package:smart_home_app/presentation/workouts/providers/workout_provider.dart';
+import 'package:smart_home_app/utils/router/router.dart';
+
+// ignore: must_be_immutable
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+  int appState = 0;
+  static final MyApp instance = MyApp._internal();
+
+  factory MyApp() => instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: AuthProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: SettingsProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: HomeProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: WorkoutProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: ConsumptionProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: ProfileProvider(),
+        )
+      ],
+      child: ScreenUtilInit(
+        builder: (context, child) => const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.getRoute,
+          initialRoute: Routes.boardingRoute,
+        ),
+        designSize: const Size(430, 810),
+      ),
+    );
+  }
+}
